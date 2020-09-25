a48265c36ede55f19d123cf75c417769091d8803
sharale34 <sourabhharale8000@gmail.com>
2020-09-25 20:11:03 +0530

    Added: Line Length Calculation Functionality is Added

--- LineComparison.java
+++ LineComparison.java
@@ -1,9 +1,19 @@
 package com.BridgeLabz.lineComparison;
-
+import java.util.Scanner;
 public class LineComparison {
    public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        System.out.println("Welcome to Line Comparison Computation Program");
+	// TODO Auto-generated method stub
+       
+     //declaration
+      Scanner sc = new Scanner(System.in);
+      int x1=sc.nextInt();
+      int x2=sc.nextInt();
+      int y1=sc.nextInt();
+      int y2=sc.nextInt();
+      
+      //computation
+      double length=Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+      System.out.println("Length of the Line: "+length);
 	}
 
 }
