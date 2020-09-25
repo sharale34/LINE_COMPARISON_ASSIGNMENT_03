0c8ca6e9fd9bdbb57d41af3c2fda4d9f8e573451
sharale34 <sourabhharale8000@gmail.com>
2020-09-25 20:03:52 +0530

    Added: Welcome Message is Added

---
+++ LineComparison.java
@@ -0,0 +1,9 @@
+package com.BridgeLabz.lineComparison;
+
+public class LineComparison {
+   public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
