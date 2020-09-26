4108f93c3804eee27781b55e6455a03d5b8ac01d
sharale34 <sourabhharale8000@gmail.com>
2020-09-26 09:38:36 +0530

    Refactor: Line Comparison Using OOPS Concepts

--- LineComparison.java
+++ LineComparison.java
@@ -1,39 +1,66 @@
 package com.BridgeLabz.lineComparison;
-import java.util.Scanner;
 public class LineComparison {
-   public static void main(String[] args) {
-	// TODO Auto-generated method stub
-       
-        //declaration
-        Scanner sc = new Scanner(System.in);
-        int x1=sc.nextInt();
-        int x2=sc.nextInt();
-        int x3=sc.nextInt();
-        int x4=sc.nextInt();
-        int y1=sc.nextInt();
-		int y2=sc.nextInt();
-		int y3=sc.nextInt();
-		int y4=sc.nextInt();
-		
-	    //computation
-		double Length1=Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
-		double Length2=Math.sqrt(((x3-x4) * (x3-x4)) + ((y4-y3) * (y4-y3)));
-		//usage of Wrapper class
-		Double obj1 = new Double(Length1);
-		Double obj2 = new Double(Length2);
-		int comp=obj1.compareTo(obj2);
-		if(comp==0)
-		{
-			System.out.println("Both the lines have equal Length.");
-		}
-		else if(comp>0)
-		{
-			System.out.println("Length of line1 is greater than line2.");
-		}
-		else
-		{
-			System.out.println("Length of line1 is less than line2.");
-		}
-	}
+	   //Decimal Formatter
+		public static boolean compOutput;
+		public static int compToOutput;
 
+		public static void main(String[] args) {
+			//Welcome message
+			System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+
+			//Variables
+			double length1, length2;
+			int x1=(int) Math.floor(Math.random()*100);
+			int x2=(int) Math.floor(Math.random()*100);
+			int y1=(int) Math.floor(Math.random()*100);
+			int y2=(int) Math.floor(Math.random()*100);
+			int x3=(int) Math.floor(Math.random()*100);
+			int x4=(int) Math.floor(Math.random()*100);
+			int y3=(int) Math.floor(Math.random()*100);
+			int y4=(int) Math.floor(Math.random()*100);
+
+			//Length computation
+			length1=Math.sqrt((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1));
+			length2=Math.sqrt((x3-x4) * (x3-x4)) + ((y3-y4) * (y3-y4));
+
+			System.out.println("Length of line 1 is : "+length1); 
+			System.out.println("Length of line 2 is : "+length2);
+
+			//Class object
+			LineComparison obj1=new LineComparison();
+			//Invoking equals and compareTo methods
+			obj1.equals(length1, length2);
+			obj1.compareTo(length1, length2);
+
+		}
+		public void equals(double Length1, double Length2) {
+			//By equals method
+			Double obj1 = new Double(Length1); 
+			Double obj2 = new Double(Length2);
+			compOutput=obj1.equals(obj2);
+
+			if(compOutput) {
+				System.out.println("Both the lines are equal in length");
+			}
+			else {
+				System.out.println("Both the lines are not equal in length");
+			}
+		}
+		public void compareTo(double Length1, double Length2) {
+			//Wrapper class
+			Double obj1 = new Double(Length1); 
+			Double obj2 = new Double(Length2);
+			compToOutput=obj1.compareTo(obj2);
+
+			if(compToOutput==0) {
+				System.out.println("Both the lines are equal in length");
+			}
+			else if(compToOutput<0){
+				System.out.println("Line 2 length is greater than line 1 length");
+			}
+			else {
+				System.out.println("Line 1 length is greater than line 2 length");
+			}
+		}
 }
+	
